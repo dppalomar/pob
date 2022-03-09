6f3137d0e0e0c8c113c8ed0acfeb3b3e71be5243
mirca <jvmirca@gmail.com>
2022-03-09 12:29:24 +0800

    setting up python package

---
+++ python/src/pob/__init__.py
@@ -0,0 +1,9 @@
+from pob.load_data import cryptos_2017to2021, SP500_2015to2020
+
+__author__ = "Daniel Palomar & Ze Vinicius"
+__email__ = "palomar@ust.hk"
+__uri__ = "https://github.com/dppalomar/pob"
+__license__ = "MIT"
+__description__ = "Supporting package for the Portfolio Optimization Book"
+__contributors__ = "https://github.com/dppalomar/pob/graphs/contributors"
+__bibtex__ = __citation__ = """TBD"""
